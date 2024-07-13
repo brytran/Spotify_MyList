c8c46abbef1765616e6835152dc7521f9dc01cc4
Bryan Tran <tranbrya@msu.edu>
2024-07-13 17:14:09 -0400

    Finished stats page

--- spotify-mylist/src/components/Stats.jsx
+++ spotify-mylist/src/components/Stats.jsx
@@ -1,6 +1,6 @@
 import Podium from "./Podium";
 import Chart from "./Chart";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getTopArtists, getTopTracks } from "../static/js/main";
 function Stats(props) {
@@ -14,6 +14,8 @@ function Stats(props) {
   const [topItems, setTopItems] = useState([]);
   const [style1, setStyle1] = useState("");
   const [style2, setStyle2] = useState("");
+  const navigate = useNavigate();
+
   useEffect(() => {
     const fetchStats = async () => {
       try {
@@ -39,12 +41,59 @@ function Stats(props) {
     });
   }, []);
 
+  function HandleRedirect() {
+    navigate("/statsselect");
+  }
+
   return (
     <>
       <div
         className="selection-container charts-header"
         style={{ background: "linear-gradient(" + color1 + "," + color2 + ")" }}
-      ></div>
+      >
+        <div
+          className=" chart-header-image"
+          style={{
+            background: color1,
+            boxShadow: "0 0 1.25rem 0.5rem " + color2,
+          }}
+        >
+          <p className="spotify-font stat-image-font top-font">Top</p>
+          <p className="spotify-font stat-image-font">{type}</p>
+          <p className="spotify-font stat-image-font duration-font">
+            {duration}
+          </p>
+        </div>
+        <div className="chart-header-text-container">
+          <p className="spotify-font stats-chart-header-font header-font-align">
+            {type == "artists"
+              ? "Top Artists " + duration
+              : "Top Tracks" + " " + duration}
+          </p>
+          <p className="spotify-font header-font-align charts-subfont">
+            {type == "artists"
+              ? "Artists you've had on repeat -" + duration
+              : "Song you've had on repeat -" + " " + duration}
+          </p>
+          <div className="spotify-font header-font-align charts-subfont">
+            <a
+              className="spotify-font header-font-align"
+              id="redirect-link"
+              onClick={HandleRedirect}
+            >
+              Top Hits
+            </a>
+            {" - 30 Entires"}
+          </div>
+          <div
+            className="spotify-font"
+            id="go-selection"
+            onClick={HandleRedirect}
+          >
+            <p id="go-selection-font">Go back</p>
+          </div>
+        </div>
+      </div>
       <div
         className="podium-gradient"
         style={{ background: "linear-gradient(" + color3 + "," + "#111312)" }}
